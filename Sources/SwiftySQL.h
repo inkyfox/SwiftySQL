cf46c2aaa4b22c1b47e86e61f0422e7f2972d89c
Yongha Yoo <inkyfox@oo-v.com>
2016-10-28 23:40:50 +0900

    Project settings

---
+++ Sources/SwiftySQL.h
@@ -0,0 +1,18 @@
+//
+//  SwiftySQL.h
+//  SwiftySQL
+//
+//  Created by Yongha Yoo (inkyfox) on 2016. 10. 26..
+//  Copyright © 2016년 Gen X Hippies Company. All rights reserved.
+//
+
+
+@import Foundation;
+
+//! Project version number for SwiftySQL.
+FOUNDATION_EXPORT double SwiftySQLVersionNumber;
+
+//! Project version string for SwiftySQL.
+FOUNDATION_EXPORT const unsigned char SwiftySQLVersionString[];
+
+
